92cae862b1a0f2d5e62ebb55b76d87fe67cae21a
Giovanni Crisalfi <giovanni.crisalfi@protonmail.com>
2023-07-03 16:29:34 +0200

    Support for simple text quoting

--- eltweet.el
+++ eltweet.el
@@ -6,10 +6,10 @@
 ;; Maintainer: gicrisf <giovanni.crisalfi@protonmail.com>
 ;; Created: marzo 15, 2022
 ;; Modified: marzo 15, 2022
-;; Version: 0.0.1
+;; Version: 0.0.5
 ;; Keywords: abbrev hypermedia tweet twitter social blog
 ;; Homepage: https://github.com/cromo/eltweet
-;; Package-Requires: ((emacs "24.3"))
+;; Package-Requires: ((emacs "24.4"))
 ;;
 ;; This file is not part of GNU Emacs.
 ;;
@@ -26,10 +26,8 @@
 
 (require 'json)
 
-;;;###autoload
-(defun eltweet-quote (uri)
-  "Quote a tweet with just the URI."
-  (interactive "sEnter URL: ")
+(defun eltweet--quote-from-uri (uri)
+  "Get a tweet with just the URI."
   (let* ((uri (concat "https://publish.twitter.com/oembed?url=" (url-hexify-string uri) "&omit_script=true"))
          (parsed (with-current-buffer (let ((buffer (url-retrieve-synchronously uri)))
                                         (unless buffer (signal 'file-error (list uri "no data")))
@@ -40,8 +38,60 @@
                    (goto-char (point-min))
                    (re-search-forward "^$")
                    (json-read))))
-    (insert (cdr (assq 'html parsed))))
+    (cdr (assq 'html parsed))))
+
+(defun eltweet--html-string-to-text (html)
+  "Convert a HTML blockquote in simple text."
+  (with-temp-buffer
+    (insert html)
+    (shr-render-region (point-min) (point-max))
+    (buffer-string)))
+
+(defun eltweet-quote (uri)
+  "Quote a tweet in your buffer with just the URI."
+  (interactive "sEnter URL: ")
+  (insert (eltweet--quote-from-uri uri))
   (message "Here is your tweet!"))
+
+(defun eltweet--indent (beg end)
+  "Indent text from BEG position to END position."
+  ;; adapted from evil-indent
+  (save-restriction
+    (narrow-to-region beg end)
+    (if (and (= beg (line-beginning-position))
+             (= end (line-beginning-position 2)))
+        ;; since some Emacs modes can only indent one line at a time,
+        ;; implement "==" as a call to `indent-according-to-mode'
+        (indent-according-to-mode)
+      (goto-char beg)
+      (indent-region beg end))
+    ;; Update `beg' and `end'
+    (setq beg (point-min)
+          end (point-max))
+    ;; We also need to tabify or untabify the leading white characters
+    (progn
+      (let* ((beg-line (line-number-at-pos beg))
+             (end-line (line-number-at-pos end))
+             (ln beg-line)
+             (convert-white (if indent-tabs-mode 'tabify 'untabify)))
+        (save-excursion
+          (while (<= ln end-line)
+            (goto-char (point-min))
+            (forward-line (- ln 1))
+            (back-to-indentation)
+            ;; Whether tab or space should be used is determined by indent-tabs-mode
+            (funcall convert-white (line-beginning-position) (point))
+            (setq ln (1+ ln))))))))
+
+(defun eltweet-quote-as-simple-text (uri)
+  "Print a tweet in simple text with just the URI."
+  (interactive "sEnter URL: ")
+  (let ((html (eltweet--quote-from-uri uri))
+        (begin-position (point)))
+    ;; (insert "#+begin_quote\n")
+    (insert (eltweet--html-string-to-text html))
+    ;; (insert "#+end_quote\n")
+    (eltweet--indent begin-position (point))))
 
 (provide 'eltweet)
 ;;; eltweet.el ends here
