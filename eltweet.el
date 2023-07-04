00cc76ace81ea2d6dbb9f9da2fdd2b05f370e1da
Giovanni Crisalfi <giovanni.crisalfi@protonmail.com>
2023-07-04 17:23:53 +0200

    Org-mode quoting supported.

--- eltweet.el
+++ eltweet.el
@@ -6,10 +6,10 @@
 ;; Maintainer: gicrisf <giovanni.crisalfi@protonmail.com>
 ;; Created: marzo 15, 2022
 ;; Modified: marzo 15, 2022
-;; Version: 0.0.5
+;; Version: 0.0.6
 ;; Keywords: abbrev hypermedia tweet twitter social blog
 ;; Homepage: https://github.com/cromo/eltweet
-;; Package-Requires: ((emacs "24.4"))
+;; Package-Requires: ((emacs "27.1"))
 ;;
 ;; This file is not part of GNU Emacs.
 ;;
@@ -25,20 +25,7 @@
 ;;; Code:
 
 (require 'json)
-
-(defun eltweet--quote-from-uri (uri)
-  "Get a tweet with just the URI."
-  (let* ((uri (concat "https://publish.twitter.com/oembed?url=" (url-hexify-string uri) "&omit_script=true"))
-         (parsed (with-current-buffer (let ((buffer (url-retrieve-synchronously uri)))
-                                        (unless buffer (signal 'file-error (list uri "no data")))
-                                        (when (fboundp 'url-http--insert-file-helper)
-                                          (url-http--insert-file-helper buffer uri))
-                                        buffer)
-                   (set-buffer-multibyte t)
-                   (goto-char (point-min))
-                   (re-search-forward "^$")
-                   (json-read))))
-    (cdr (assq 'html parsed))))
+(require 'org)
 
 (defun eltweet--html-string-to-text (html)
   "Convert a HTML blockquote in simple text."
@@ -46,12 +33,6 @@
     (insert html)
     (shr-render-region (point-min) (point-max))
     (buffer-string)))
-
-(defun eltweet-quote (uri)
-  "Quote a tweet in your buffer with just the URI."
-  (interactive "sEnter URL: ")
-  (insert (eltweet--quote-from-uri uri))
-  (message "Here is your tweet!"))
 
 (defun eltweet--indent (beg end)
   "Indent text from BEG position to END position."
@@ -83,15 +64,87 @@
             (funcall convert-white (line-beginning-position) (point))
             (setq ln (1+ ln))))))))
 
+(defun eltweet--quote-from-uri (uri)
+  "Get a tweet with just the URI."
+  (let* ((uri (concat "https://publish.twitter.com/oembed?url=" (url-hexify-string uri) "&omit_script=true"))
+         (parsed (with-current-buffer (let ((buffer (url-retrieve-synchronously uri)))
+                                        (unless buffer (signal 'file-error (list uri "no data")))
+                                        (when (fboundp 'url-http--insert-file-helper)
+                                          (url-http--insert-file-helper buffer uri))
+                                        buffer)
+                   (set-buffer-multibyte t)
+                   (goto-char (point-min))
+                   (re-search-forward "^$")
+                   (json-read))))
+    parsed))
+
+(defun eltweet-quote-as-html (uri)
+  "Quote a tweet in your buffer with just the URI."
+  (interactive "sEnter url: ")
+  (insert (cdr (assq 'html (eltweet--quote-from-uri uri))))
+  (message "here is your tweet!"))
+
 (defun eltweet-quote-as-simple-text (uri)
   "Print a tweet in simple text with just the URI."
   (interactive "sEnter URL: ")
-  (let ((html (eltweet--quote-from-uri uri))
+  (let ((html (cdr (assq 'html (eltweet--quote-from-uri uri))))
         (begin-position (point)))
     ;; (insert "#+begin_quote\n")
     (insert (eltweet--html-string-to-text html))
     ;; (insert "#+end_quote\n")
     (eltweet--indent begin-position (point))))
+
+(defun eltweet--get-href (link)
+  "Extract the href value of a parsed LINK."
+  (cdr (car (car (cdr link)))))
+
+(defun eltweet--orgwriter (strings links)
+  "Read into STRINGS and LINKS of a parsed tweet to write a well-formed Org one."
+  (when strings
+    (if (< (length strings) 3)
+        (when links
+          (if (= (length links) 1)
+              ;; 1 link left
+              (progn
+                (insert (concat "\n" (org-link-make-string (eltweet--get-href (car links)) (car strings))) "\n")
+                (eltweet--orgwriter (cdr strings) (cdr links)))
+            ;; links are not 1
+            (progn
+              (let ((link-href (eltweet--get-href (car links))))
+                (insert (concat (org-link-make-string link-href link-href) "\n")))
+              (eltweet--orgwriter strings (cdr links)))))
+      ;; Strings > 2 case
+      (let* ((body-string (car strings))
+             (type (url-type (url-generic-parse-url body-string))))
+        ;; Search for URLs
+        (if (or (equal type "https") (equal type "http"))
+            (message "Skipping URL %s" body-string)
+          ;; Print non-URL strings
+          (insert (concat body-string "\n")))
+        (eltweet--orgwriter (cdr strings) links)))))
+
+(defun eltweet--parse-html-string (html)
+  "Parse an HTML string and return the parsed result."
+  (with-temp-buffer
+    (insert html)
+    (libxml-parse-html-region (point-min) (point-max))))
+
+(defun eltweet-quote-as-org (uri)
+  "Quote a tweet in your buffer with just the URI."
+  (interactive "sEnter url: ")
+  ;; (insert (cdr (assq 'html (eltweet--quote-from-uri uri))))
+  (let* ((html (cdr (assq 'html (eltweet--quote-from-uri uri))))
+         (html-tree (eltweet--parse-html-string html))
+         (begin-position (point))
+         (strings (dom-strings html-tree))
+         (links (dom-by-tag html-tree 'a)))
+    (insert "#+begin_quote\n")
+    (insert (with-temp-buffer
+              (orgwriter strings links)
+              (buffer-string)))
+    (insert "#+end_quote\n")
+    (eltweet--indent begin-position (point)))
+  (message "Here is your tweet!"))
 
 (provide 'eltweet)
 ;;; eltweet.el ends here
